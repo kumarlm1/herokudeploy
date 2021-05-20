26e87fae2090cd957e3def1b7402fe91a216093f
kumarlm1 <74861509+kumarlm1@users.noreply.github.com>
2021-05-20 18:26:09 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1_k_s*9q7*l+m-6*k9ubgh)j5oppv#yi@^eyxcfd%*s)uhn2z^'
+#SECRET_KEY = '1_k_s*9q7*l+m-6*k9ubgh)j5oppv#yi@^eyxcfd%*s)uhn2z^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://djangosdeploytest.herokuapp.com/']
 
 
 # Application definition
